532ce596cfb9f0085e4b4c33de7d894972e35fde
Dabana Intenque <Dabanaluintenque55@gmail.com>
2024-03-16 01:49:20 +0000

    week4 coding challenge is Sorted
    Done!

--- src/main/java/IsSorted.java
+++ src/main/java/IsSorted.java
@@ -7,6 +7,21 @@ public class IsSorted {
      * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
      */
     public boolean checkSort(int[] arr){
-        return false;
+
+        boolean result = false;
+
+       for(int i = 0; i < arr.length -1 ; i ++) {
+
+            if( arr[i] > arr[i + 1]){
+
+                result = false;
+                break;
+            }
+
+            else {
+                result = true;
+            }
+       }
+       return result;
     }
 }
